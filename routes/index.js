c8efa4b3c898f18b807319275e91097950164a3f
JoeG05 <jgualdar@ramapo.edu>
2017-12-06 16:32:38 -0500

    Add thanks route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/contacts';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,5 +13,8 @@ router.get("/mailer", function(req, res, next) {
   res.render("mailer");
 });
 
+router.post("/mailer", function(req, res, next) {
+  res.render("thanks");
+})
 
 module.exports = router;
