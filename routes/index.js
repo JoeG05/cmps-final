53664f3c30f5f0e4be8b9d7a1900ff274dd88afe
JoeG05 <jgualdar@ramapo.edu>
2017-12-12 15:23:03 -0500

    Add geolocation

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+
 var MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 var dbuser = process.env.DB_USER;
 var dbpass = process.env.DB_PASSWORD;
 var url = 'mongodb://' + dbuser + ':' + dbpass + '@ds133856.mlab.com:33856/final';
+
+var NodeGeocoder = require('node-geocoder');
+var options = {
+  provider: 'google',
+  apiKey: 'AIzaSyANpmFb2U9rjGAIaClkoTMJms8iOUE67XA',
+  formatter: null
+};
+
+var geocoder = NodeGeocoder(options);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,7 +31,26 @@ router.post("/mailer", function(req, res, next) {
   
   MongoClient.connect(url, function(err, db) {
     var dbase=db.db("final");
-    dbase.collection("contacts").insertOne(req.body, function(err, res) {
+    var contact = {};
+    var name = req.body.firstName + " " + req.body.lastName;
+    contact.address = req.body.street + " " + req.body.city + " " + req.body.state + " " + req.body.zip;
+    if (req.body.title) {
+      contact.name=req.body.title + " " + name;
+    } else {
+      contact.name=name;
+    }
+    contact.phone=req.body.phone;
+    contact.email=req.body.email;
+    contact.cbPhone=req.body.cbPhone;
+    contact.cbEmail=req.body.cbEmail;
+    contact.cbMail=req.body.cbMail;
+    contact.any=req.body.any;
+    geocoder.geocode(contact.address, function(err, res) {
+      contact.lat=res[0].latitude;
+      contact.long=res[0].longitude;
+      console.log(contact);
+    });
+    dbase.collection("contacts").insertOne(contact, function(err, res) {
       if (err) throw err;
       console.log("1 record inserted");
       db.close();
