6415b766253343f623f142efc0c4238ce926595a
JoeG05 <jgualdar@ramapo.edu>
2017-12-13 00:58:05 -0500

    Map positioned.

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,12 @@
 var map;
 
 function initMap() {
-    
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.7570877, lng: -73.8458213},
         zoom: 8
     });
+}
+
+function addMarker(x) {
+    alert(x);
 }
